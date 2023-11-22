adb78123e39904ad20a172620274e3d7cea1624f
Tyson Freeze <tyson.freeze@icloud.com>
2023-11-21 17:36:00 -0700

    add sob_story

---
+++ migrations/20231122001600_v5.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('gift', function (table) {
+        table.string('sob_story');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('gift', function (table) {
+        table.dropColumn('sob_story');
+    })
+};
