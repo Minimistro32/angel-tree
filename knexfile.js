7df1648a8ef6d0ec62c80dcc9f5ddcc39d94a61e
Tyson Freeze <tyson.freeze@icloud.com>
2023-11-22 16:17:39 -0700

    fix ssl

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,10 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       min: 2,
