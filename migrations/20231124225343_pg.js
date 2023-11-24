1c63b4d4fc1b95c637e58c639da861100ca49ca5
Tyson Freeze <tyson.freeze@icloud.com>
2023-11-24 16:09:39 -0700

    update length

--- migrations/20231124225343_pg.js
+++ migrations/20231124225343_pg.js
@@ -4,9 +4,11 @@
  */
 exports.up = function(knex) {
     return knex.schema.alterTable('gift', (table) =>  {
-        table.text('desc', 1000).alter();
-        table.text('title', 1000).alter();
-        table.text('sob_story', 1000).alter();
+        table.string('desc', 1000).alter();
+        table.string('title', 1000).alter();
+        table.string('sob_story', 1000).alter();
+        table.string('suggest_url', 1000).alter();
+        table.string('url', 1000).alter();
     });
 };
 
@@ -19,5 +21,7 @@ exports.down = function(knex) {
         table.string('desc').alter();
         table.string('title').alter();
         table.string('sob_story').alter();
+        table.string('suggest_url').alter();
+        table.string('url').alter();
     });
 };
